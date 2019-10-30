60666cfdfc44ed5939a7ad467160ca19b651e24a
thai056 <jearananthai@gmail.com>
2019-10-30 20:56:05 +0700

    commit

---
+++ Training/index.js
@@ -0,0 +1,17 @@
+console.log("Hello Wolrd");
+
+//var x;   global 
+//let y; สามารถแก้ไขได้
+//const z;   เป็นค่าคงที่ เปลี่ยนค่าคงที่ไม่ได้
+
+//let x =10; ตัวเลข
+//y = 'c' "c" `c` ข้อความ alt + 96
+
+//let array = [2, 3, 6]   array[0] = 2  
+
+// let obj = { a:2, b:3}   obj['a'] หรือ obj.a
+
+//function fn(a, b)  {return a+b;} console.log(fn(10, 20)) ฟังก์ชันเก็บในตัวแปรได้
+//const fn2 = (a, b) => {return a-b;} /*(arrow fn)*/ console.log(fn2(10, 20))
+
+//มีข้อแตกต่างระหว่าง next.js -> ง่ายกว่า & REACT -> ยากอยู่
